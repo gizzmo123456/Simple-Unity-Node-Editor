19e4c3a22d263315e9d8eb30ffa61c019a605696
Ashley Sands <ashsands1@amstrike.com>
2019-12-29 18:34:55 +0000

    added basic node graph window

---
+++ Editor/BaseNodeGraphEditor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BaseNodeGraphEditor : BaseNodeEditor<BaseNodeGraphData>
+{
+	public BaseNodeGraphEditor () : base() { }
+	public BaseNodeGraphEditor (Rect pannelRect) : base(pannelRect) { }
+
+	protected override Vector2 NodeSize ()
+	{
+		return new Vector2( 75, 50 );
+	}
+
+	public override BaseNodeGraphData AddNode ( string title, bool isDragable )
+	{
+		BaseNodeGraphData data = new BaseNodeGraphData()
+		{
+			dragable = isDragable,
+			title = title
+		};
+
+		return AddNode( data );
+
+	}
+
+	public override BaseNodeGraphData AddNode ( BaseNodeGraphData data )	// TODO: move into baseNodeEditor.
+	{
+		data.SetNodePosition( NodeStartPosition() );
+		data.SetNodeSize( NodeSize() );
+
+		nodes.Add( data );
+
+		return data;
+	}
+
+	protected override Rect ClampNodePosition ( Rect nodeRect, int winId = 0 )	// TODO: should be default?
+	{
+		return nodeRect;
+	}
+
+	protected override void NodeWindow ( int windowId )
+	{
+		base.NodeWindow( windowId );
+	}
+
+
+}
+
+public class BaseNodeGraphData : BaseNodeData
+{
+
+}
--- Editor/NodeWindow.cs
+++ Editor/NodeWindow.cs
@@ -13,28 +13,35 @@ public class NodeWindow : EditorWindow
 	}
 
 	BaseVerticalEditor nodeWindow;
+	BaseNodeGraphEditor nodeGraphEditor;
 
 	private void Awake ()
 	{
 
-		nodeWindow = new BaseVerticalEditor(new Rect(0, 0, 500, 500));
-		nodeWindow.AddNode("Win 1", true);
-		nodeWindow.AddNode("Win 2", true);
-		nodeWindow.AddNode("Win 3", true);
-		nodeWindow.AddNode("Win 4", true);
-		nodeWindow.AddNode("Win 5", true);
+		//nodeWindow = new BaseVerticalEditor(new Rect(0, 0, 500, 500));
+		//nodeWindow.AddNode("Win 1", true);
+		//nodeWindow.AddNode("Win 2", true);
+		//nodeWindow.AddNode("Win 3", true);
+		//nodeWindow.AddNode("Win 4", true);
+		//nodeWindow.AddNode("Win 5", true);
+
+		nodeGraphEditor = new BaseNodeGraphEditor(new Rect(0, 500, 500, 500));
+		nodeGraphEditor.AddNode( "Node 1", true );
+		nodeGraphEditor.AddNode( "Node 2", true );
+		nodeGraphEditor.AddNode( "Node 3", true );
 
 	}
 
 	private void Update ()
 	{
-		nodeWindow.Update();
+		//nodeWindow.Update();
 	}
 
 	private void OnGUI ()
 	{
 		
-		nodeWindow.Draw(this);
+		//nodeWindow.Draw(this);
+		nodeGraphEditor.Draw(this);
 	}
 
 
