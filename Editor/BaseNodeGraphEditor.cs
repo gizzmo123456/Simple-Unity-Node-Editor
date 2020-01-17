83a2676ade284fb99aa8d9dacbe66fe56d29dd58
Ashley Sands <ashsands1@amstrike.com>
2020-01-17 15:10:09 +0000

    removed destructor

--- Editor/BaseNodeGraphEditor.cs
+++ Editor/BaseNodeGraphEditor.cs
@@ -24,11 +24,6 @@ public abstract class BaseNodeGraphEditor<T> : BaseNodeEditor<T> where T : BaseN
 		nodeReleased += NodeReleased;	
 	}
 
-	~BaseNodeGraphEditor ()
-	{
-		nodeReleased -= NodeReleased;
-	}
-
 	public override void Draw ( EditorWindow window )
 	{
 		base.Draw( window );
